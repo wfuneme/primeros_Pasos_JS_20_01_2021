0c1b728514bd81b6a7d5caa96685f0caab21b341
Willmer Obed Fúneme San Martín <willmer.funeme@unac.edu.co>
2021-01-20 19:30:31 -0500

    Tipos de Datos

---
+++ main.js
@@ -0,0 +1,37 @@
+// Tipos de datos
+// string cadena
+
+"Willmer Funeme"
+
+
+// number   número
+1000
+2.5
+2,2
+-25
+
+// boolean   afirmaciones
+true
+false
+
+
+// array    listas
+['angel', 'fabian', 'andres']
+[1, 2, 3, 4]
+
+
+// object   objeto
+{
+    nombre: "Willmer",
+    apellido: "Funeme",
+    edad: 23,
+    profesor: true,
+}
+
+{
+    nombre: "Diana",
+    apellido:"Bautista",
+    edad:15,
+    profesor: false
+
+}
